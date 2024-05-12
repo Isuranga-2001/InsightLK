2342441e28c13cce2b1099a6d753d57c8b8b4807
Kavindu Fernando <kavindufernando329@gmail.com>
2024-05-12 23:32:39 +0530

    fix - Update conversation chain model to use ChatOpenAI instead of HuggingFaceHub

--- AI_model/scripts/get_conversation_chain_from_embedded_text.py
+++ AI_model/scripts/get_conversation_chain_from_embedded_text.py
@@ -5,8 +5,8 @@ from langchain.chat_models import ChatOpenAI
 
 
 def get_conversation_chain(vectorstore):
-    # llm = ChatOpenAI()
-    llm = HuggingFaceHub(repo_id="google/flan-t5-xl", model_kwargs={"temperature":0.5, "max_length":512})
+    llm = ChatOpenAI()
+    # llm = HuggingFaceHub(repo_id="google/flan-t5-large", model_kwargs={"temperature":0.5, "max_length":512})
 
     memory = ConversationBufferMemory(
         memory_key='chat_history', return_messages=True)
